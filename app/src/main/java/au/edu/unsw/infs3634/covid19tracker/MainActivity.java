200eccc3123e6400031947b66e65411abca18e6e
Mery Fesen <71361278+Meryfesen@users.noreply.github.com>
2021-09-22 16:46:28 +1000

    week 2

--- app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/covid19tracker/MainActivity.java
@@ -2,37 +2,13 @@ package au.edu.unsw.infs3634.covid19tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // logging
-        Log.d(TAG, "This is the example of lagging");
-
-        // debugging
-        int num1 = 100, num2 = 200;
-        int total = num1 + num2;
-        Button btn = findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView textView = findViewById(R.id.textView);
-                textView.setText("This message came from MainActivity");
-                Intent intent = new Intent(MainActivity.this, SecondaryActivity2.class);
-                startActivity(intent);
-            }
-        });
     }
-
 }
